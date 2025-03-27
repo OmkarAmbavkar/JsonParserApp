ef7e3182a0b265f515f6b396c3cccad5a550638e
Omkae12 <aumkar.7420@gmail.com>
2025-03-27 16:30:12 +0530

    JsonParserApp.java

--- JsonParserApp.java
+++ JsonParserApp.java
@@ -1,18 +1,15 @@
 import java.nio.file.*;
 import java.security.*;
 import java.math.*;
-import org.json.JSONObject;
 
 public class JsonParserApp {
     public static void main(String[] args) {
 
             String x = new String(Paths.get("input.json"));
-            JSONObject a = new JSONObject(x).getJSONObject("student");
             String name = a.getString("first_name").toLowerCase();
             String roll = a.getString("roll_number").toLowerCase();
             String b = name + roll;
             String ans = output(b);
-            Files.write(Paths.get("output.txt"), ans.getBytes());
             System.out.println("Final output: " + ans);
     }
 
